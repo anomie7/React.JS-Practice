82df3eca234c706fb14fec3a10f18d226708082d
minu youn <anomie7777@gmail.com>
2017-09-25 19:44:41 +0900

    최초 커밋

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ class App extends React.Component {
     render(){
 
         return (
-                <h1>Hello React Skeleton</h1>
+                <Header />
+                <Content />
         );
     }
 }
