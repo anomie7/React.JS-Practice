71d071032a5abe02368db69b65627e7019c3cf72
minu youn <anomie7777@gmail.com>
2017-09-25 20:56:43 +0900

    [ReactJS_Practice] state를 이용한 랜덤값 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from './Header';
 import Content from './Content';
+import RandomNumber from './RandomNumber';
+
 
 class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            value: Math.round(Math.random()*100)
+        };
+
+        this.updateValue = this.updateValue.bind(this);
+    }
+
+    updateValue(randomValue){
+        this.setState({
+            value: randomValue
+        });
+    }
+
     render(){
 
         return (
@@ -10,6 +30,8 @@ class App extends React.Component {
                 <Header title={this.props.headerTitle}/>
                 <Content title={this.props.contentTitle}
                          body = {this.props.contentBody}/>
+                <RandomNumber number={this.state.value}
+                              onUpdate={this.updateValue} />
             </div>
         );
     }
